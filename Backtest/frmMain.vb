21b24abd6bd721b702a0bda6d1f3e1791430c37f
Indibar <indibar@algo2trade.com>
2019-09-25 15:27:39 +0530

    Check Point

--- Backtest/frmMain.vb
+++ Backtest/frmMain.vb
@@ -286,14 +286,14 @@ Public Class frmMain
                     tick = 0.05
             End Select
 
-            For signalType As Integer = 13 To 14 Step 1
-                For strtLvlMul As Integer = 1 To 2 Step 1
-                    For chngLvlAfterSl As Integer = 0 To 1 Step 1
+            For signalType As Integer = 7 To 7 Step 1
+                For strtLvlMul As Integer = 1 To 1 Step 1
+                    For chngLvlAfterSl As Integer = 0 To 0 Step 1
                         For aftrSLLvlMul As Integer = 2 To 2 Step 1
                             For nmbrOfTradePerStock As Integer = 20 To 20 Step 1
                                 For mdfyNmbrOfTrd As Integer = 0 To 0 Step 1
-                                    For overallMaxLoss As Decimal = -8000 To -9000 Step -1000
-                                        For brkEvnMvmnt As Integer = 0 To 1 Step 1
+                                    For overallMaxLoss As Decimal = -9000 To -9000 Step -1000
+                                        For brkEvnMvmnt As Integer = 1 To 1 Step 1
                                             For tradeMaxProfit As Decimal = Decimal.MaxValue To Decimal.MaxValue Step -1
                                                 If brkEvnMvmnt = 1 AndAlso tradeMaxProfit <> Decimal.MaxValue Then Continue For
                                                 For stockMaxProfit As Decimal = Decimal.MaxValue To Decimal.MaxValue Step -1
