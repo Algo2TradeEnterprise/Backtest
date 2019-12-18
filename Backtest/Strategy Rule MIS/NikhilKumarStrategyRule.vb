46b48ff7dc9603b135e4638519bf185b24af893c
Indibar <indibar@algo2trade.com>
2019-12-18 09:45:12 +0530

    During fix

---
+++ Backtest/Strategy Rule MIS/NikhilKumarStrategyRule.vb
@@ -0,0 +1,37 @@
+﻿Imports Backtest.StrategyHelper
+Imports System.Threading
+Imports Algo2TradeBLL
+
+Public Class NikhilKumarStrategyRule
+    Inherits StrategyRule
+
+    Public Sub New(ByVal inputPayload As Dictionary(Of Date, Payload),
+                   ByVal lotSize As Integer,
+                   ByVal parentStrategy As Strategy,
+                   ByVal tradingDate As Date,
+                   ByVal tradingSymbol As String,
+                   ByVal canceller As CancellationTokenSource,
+                   ByVal entities As RuleEntities)
+        MyBase.New(inputPayload, lotSize, parentStrategy, tradingDate, tradingSymbol, canceller, entities)
+    End Sub
+
+    Public Overrides Function IsTriggerReceivedForPlaceOrderAsync(currentTick As Payload) As Task(Of Tuple(Of Boolean, List(Of PlaceOrderParameters)))
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function IsTriggerReceivedForExitOrderAsync(currentTick As Payload, currentTrade As Trade) As Task(Of Tuple(Of Boolean, String))
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function IsTriggerReceivedForExitCNCEODOrderAsync(currentTick As Payload, currentTrade As Trade) As Task(Of Tuple(Of Boolean, Decimal, String))
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function IsTriggerReceivedForModifyStoplossOrderAsync(currentTick As Payload, currentTrade As Trade) As Task(Of Tuple(Of Boolean, Decimal, String))
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function IsTriggerReceivedForModifyTargetOrderAsync(currentTick As Payload, currentTrade As Trade) As Task(Of Tuple(Of Boolean, Decimal, String))
+        Throw New NotImplementedException()
+    End Function
+End Class
