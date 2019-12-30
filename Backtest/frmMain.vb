17f292854bef31fc6c64a3f0c1f09ed6dc8f5237
Indibar <indibar@algo2trade.com>
2019-12-30 13:33:15 +0530

    After fix

--- Backtest/frmMain.vb
+++ Backtest/frmMain.vb
@@ -279,7 +279,7 @@ Public Class frmMain
             Select Case stockType
                 Case Trade.TypeOfStock.Cash
                     database = Common.DataBaseTable.Intraday_Cash
-                    margin = 13
+                    margin = 10
                     tick = 0.05
                 Case Trade.TypeOfStock.Commodity
                     database = Common.DataBaseTable.Intraday_Commodity
