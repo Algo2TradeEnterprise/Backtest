adfac9eaae7ff0c00c6af264e66a0e76efa669b2
Indibar <indibar@algo2trade.com>
2020-02-17 15:58:05 +0530

    After fix

--- Backtest/frmMain.vb
+++ Backtest/frmMain.vb
@@ -1407,7 +1407,7 @@ Public Class frmMain
 
                     .AllowBothDirectionEntryAtSameTime = False
                     .TrailingStoploss = False
-                    .TickBasedStrategy = True
+                    .TickBasedStrategy = False
                     .RuleNumber = GetComboBoxIndex_ThreadSafe(cmbRule)
                     .RuleEntityData = New FractalDipStrategyRule.StrategyRuleEntities With
                         {
