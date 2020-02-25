959094d9fcfcca1fa71b7729fe829b92c5f28a18
Indibar <indibar@algo2trade.com>
2020-02-25 15:28:57 +0530

    After fix

--- Backtest/frmMain.vb
+++ Backtest/frmMain.vb
@@ -299,7 +299,7 @@ Public Class frmMain
                     tick = 0.05
             End Select
 
-#Region "Pair Strategy Rule"
+#Region "Pair Change Percent Strategy Rule"
             Using backtestStrategy As New MISGenericStrategy(canceller:=_canceller,
                                                               exchangeStartTime:=TimeSpan.Parse("09:15:00"),
                                                               exchangeEndTime:=TimeSpan.Parse("15:29:59"),
