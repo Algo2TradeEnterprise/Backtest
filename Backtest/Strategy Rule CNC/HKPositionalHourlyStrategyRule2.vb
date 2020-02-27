671a69a05b534dcee001f61a52fab04f2de834f0
Indibar <indibar@algo2trade.com>
2020-02-27 18:54:36 +0530

    After fix

--- Backtest/Strategy Rule CNC/HKPositionalHourlyStrategyRule2.vb
+++ Backtest/Strategy Rule CNC/HKPositionalHourlyStrategyRule2.vb
@@ -7,6 +7,9 @@ Public Class HKPositionalHourlyStrategyRule2
     Inherits StrategyRule
 
     Private _hkPayload As Dictionary(Of Date, Payload)
+    Private _entryForContractRolloverDirection As Trade.TradeExecutionDirection = Trade.TradeExecutionDirection.None
+    Private _exitForContractRollover As Boolean
+    Private _previousSymbolPayloads As Dictionary(Of Date, Payload)
 
     Public Sub New(ByVal inputPayload As Dictionary(Of Date, Payload),
                    ByVal lotSize As Integer,
@@ -22,11 +25,22 @@ Public Class HKPositionalHourlyStrategyRule2
         MyBase.CompletePreProcessing()
 
         Indicator.HeikenAshi.ConvertToHeikenAshi(_signalPayload, _hkPayload)
+
+        Dim previousTradingDay As Date = _parentStrategy.Cmn.GetPreviousTradingDay(_parentStrategy.DatabaseTable, _tradingDate)
+        If previousTradingDay <> Date.MinValue Then
+            Dim rawInstrument As String = _tradingSymbol.Remove(_tradingSymbol.Count - 8)
+            Dim previousTradingDaySymbol As String = _parentStrategy.Cmn.GetCurrentTradingSymbol(_parentStrategy.DatabaseTable, previousTradingDay, rawInstrument)
+            If previousTradingDaySymbol.ToUpper <> _tradingSymbol.ToUpper Then
+                _exitForContractRollover = True
+                _previousSymbolPayloads = _parentStrategy.Cmn.GetRawPayloadForSpecificTradingSymbol(_parentStrategy.DatabaseTable, previousTradingDaySymbol, previousTradingDay, previousTradingDay)
+            End If
+        End If
     End Sub
 
     Public Overrides Async Function IsTriggerReceivedForPlaceOrderAsync(currentTick As Payload) As Task(Of Tuple(Of Boolean, List(Of PlaceOrderParameters)))
         Dim ret As Tuple(Of Boolean, List(Of PlaceOrderParameters)) = Nothing
         Await Task.Delay(0).ConfigureAwait(False)
+        If _exitForContractRollover Then ExitForContractRollover(currentTick)
         Dim currentMinuteCandlePayload As Payload = _signalPayload(_parentStrategy.GetCurrentXMinuteCandleTime(currentTick.PayloadDate, _signalPayload))
         Dim tradeStartTime As Date = New Date(_tradingDate.Year, _tradingDate.Month, _tradingDate.Day, _parentStrategy.TradeStartTime.Hours, _parentStrategy.TradeStartTime.Minutes, _parentStrategy.TradeStartTime.Seconds)
 
@@ -35,7 +49,7 @@ Public Class HKPositionalHourlyStrategyRule2
             Not _parentStrategy.IsTradeOpen(currentTick, _parentStrategy.TradeType) AndAlso Not _parentStrategy.IsTradeActive(currentTick, _parentStrategy.TradeType) Then
             Dim signalCandle As Payload = Nothing
 
-            Dim signal As Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection) = GetSignalForEntry(currentTick)
+            Dim signal As Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection, String) = GetSignalForEntry(currentTick)
             If signal IsNot Nothing AndAlso signal.Item1 Then
                 signalCandle = signal.Item3
                 If signalCandle IsNot Nothing Then
@@ -50,7 +64,8 @@ Public Class HKPositionalHourlyStrategyRule2
                                         .Target = .EntryPrice + 1000000,
                                         .Buffer = buffer,
                                         .SignalCandle = signalCandle,
-                                        .OrderType = Trade.TypeOfOrder.Market
+                                        .OrderType = Trade.TypeOfOrder.Market,
+                                        .Supporting1 = signal.Item5
                                     }
                     ElseIf signal.Item4 = Trade.TradeExecutionDirection.Sell Then
                         parameter = New PlaceOrderParameters With {
@@ -61,14 +76,16 @@ Public Class HKPositionalHourlyStrategyRule2
                                         .Target = .EntryPrice - 1000000,
                                         .Buffer = buffer,
                                         .SignalCandle = signalCandle,
-                                        .OrderType = Trade.TypeOfOrder.Market
+                                        .OrderType = Trade.TypeOfOrder.Market,
+                                        .Supporting1 = signal.Item5
                                     }
                     End If
                 End If
-                End If
+            End If
         End If
         If parameter IsNot Nothing Then
             ret = New Tuple(Of Boolean, List(Of PlaceOrderParameters))(True, New List(Of PlaceOrderParameters) From {parameter})
+            _entryForContractRolloverDirection = Trade.TradeExecutionDirection.None
         End If
         Return ret
     End Function
@@ -76,19 +93,6 @@ Public Class HKPositionalHourlyStrategyRule2
     Public Overrides Async Function IsTriggerReceivedForExitOrderAsync(currentTick As Payload, currentTrade As Trade) As Task(Of Tuple(Of Boolean, String))
         Dim ret As Tuple(Of Boolean, String) = Nothing
         Await Task.Delay(0).ConfigureAwait(False)
-        'If currentTrade IsNot Nothing AndAlso currentTrade.TradeCurrentStatus = Trade.TradeExecutionStatus.Open Then
-        '    Dim signalReceivedForEntry As Tuple(Of Boolean, Decimal, Payload) = GetSignalForEntry(currentTick)
-        '    If signalReceivedForEntry IsNot Nothing AndAlso signalReceivedForEntry.Item1 Then
-        '        If currentTrade.SignalCandle.PayloadDate <> signalReceivedForEntry.Item3.PayloadDate Then
-        '            ret = New Tuple(Of Boolean, String)(True, "Invalid Signal")
-        '        End If
-        '    Else
-        '        Dim currentMinuteCandlePayload As Payload = _signalPayload(_parentStrategy.GetCurrentXMinuteCandleTime(currentTick.PayloadDate, _signalPayload))
-        '        If currentMinuteCandlePayload.Close < _smaPayload(currentMinuteCandlePayload.PayloadDate) Then
-        '            ret = New Tuple(Of Boolean, String)(True, "Invalid Signal")
-        '        End If
-        '    End If
-        'End If
         Return ret
     End Function
 
@@ -118,21 +122,43 @@ Public Class HKPositionalHourlyStrategyRule2
         Return ret
     End Function
 
-#Region "Entry Rule"
-    Private Function GetSignalForEntry(ByVal currentTick As Payload) As Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection)
-        Dim ret As Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection) = Nothing
+    Private Function GetSignalForEntry(ByVal currentTick As Payload) As Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection, String)
+        Dim ret As Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection, String) = Nothing
         Dim currentMinuteCandlePayload As Payload = _signalPayload(_parentStrategy.GetCurrentXMinuteCandleTime(currentTick.PayloadDate, _signalPayload))
         Dim currentHKPayload As Payload = _hkPayload(currentMinuteCandlePayload.PayloadDate)
         If currentTick.PayloadDate >= currentHKPayload.PayloadDate.AddMinutes(59) OrElse
             currentTick.PayloadDate >= New Date(currentTick.PayloadDate.Year, currentTick.PayloadDate.Month, currentTick.PayloadDate.Day, 15, 29, 0) Then
             If currentHKPayload.Open - 1 <= currentHKPayload.Low Then
-                ret = New Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection)(True, currentTick.Open, currentHKPayload, Trade.TradeExecutionDirection.Buy)
+                ret = New Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection, String)(True, currentTick.Open, currentHKPayload, Trade.TradeExecutionDirection.Buy, "Normal Entry")
             ElseIf currentHKPayload.Open + 1 >= currentHKPayload.High Then
-                ret = New Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection)(True, currentTick.Open, currentHKPayload, Trade.TradeExecutionDirection.Sell)
+                ret = New Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection, String)(True, currentTick.Open, currentHKPayload, Trade.TradeExecutionDirection.Sell, "Normal Entry")
             End If
+        ElseIf _entryForContractRolloverDirection <> Trade.TradeExecutionDirection.None Then
+            ret = New Tuple(Of Boolean, Decimal, Payload, Trade.TradeExecutionDirection, String)(True, currentTick.Open, currentHKPayload, _entryForContractRolloverDirection, "Contract Rollover Entry")
         End If
         Return ret
     End Function
-#End Region
+
+    Private Sub ExitForContractRollover(ByVal currentTick As Payload)
+        If _exitForContractRollover Then
+            Dim currentCandlePayload As Payload = _previousSymbolPayloads(_parentStrategy.GetCurrentXMinuteCandleTime(currentTick.PayloadDate, _previousSymbolPayloads))
+            Dim potentialRuleExitTrades As List(Of Trade) = _parentStrategy.GetSpecificTrades(currentCandlePayload, Trade.TypeOfTrade.CNC, Trade.TradeExecutionStatus.Inprogress)
+            If potentialRuleExitTrades IsNot Nothing AndAlso potentialRuleExitTrades.Count > 0 Then
+                For Each runningExitTrade In potentialRuleExitTrades
+                    Dim exitTick As Payload = New Payload(Payload.CandleDataSource.Calculated) With {
+                                                        .Open = currentCandlePayload.Open,
+                                                        .Low = currentCandlePayload.Open,
+                                                        .High = currentCandlePayload.Open,
+                                                        .Close = currentCandlePayload.Open,
+                                                        .PayloadDate = currentCandlePayload.PayloadDate,
+                                                        .TradingSymbol = currentCandlePayload.TradingSymbol
+                                                    }
+                    _parentStrategy.ExitTradeByForce(runningExitTrade, exitTick, "Contract Rollover Exit")
+                Next
+                _exitForContractRollover = False
+                _entryForContractRolloverDirection = potentialRuleExitTrades.LastOrDefault.EntryDirection
+            End If
+        End If
+    End Sub
 
 End Class
