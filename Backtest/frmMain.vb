a1f301df3ab5287f2a87d628d2e269e6b27309ce
Indibar <indibar@algo2trade.com>
2020-03-03 15:35:06 +0530

    After fix

--- Backtest/frmMain.vb
+++ Backtest/frmMain.vb
@@ -1692,66 +1692,10 @@ Public Class frmMain
 #End Region
 
 #Region "HK Slab Level"
-            Using backtestStrategy As New MISGenericStrategy(canceller:=_canceller,
-                                                              exchangeStartTime:=TimeSpan.Parse("09:15:00"),
-                                                              exchangeEndTime:=TimeSpan.Parse("15:29:59"),
-                                                              tradeStartTime:=TimeSpan.Parse("9:20:00"),
-                                                              lastTradeEntryTime:=TimeSpan.Parse("14:45:59"),
-                                                              eodExitTime:=TimeSpan.Parse("15:15:00"),
-                                                              tickSize:=tick,
-                                                              marginMultiplier:=margin,
-                                                              timeframe:=1,
-                                                              heikenAshiCandle:=False,
-                                                              stockType:=stockType,
-                                                              databaseTable:=database,
-                                                              dataSource:=sourceData,
-                                                              initialCapital:=Decimal.MaxValue / 2,
-                                                              usableCapital:=Decimal.MaxValue / 2,
-                                                              minimumEarnedCapitalToWithdraw:=Decimal.MaxValue,
-                                                              amountToBeWithdrawn:=0)
-                AddHandler backtestStrategy.Heartbeat, AddressOf OnHeartbeat
-
-                With backtestStrategy
-                    .StockFileName = Path.Combine(My.Application.Info.DirectoryPath, "ATR Stocklist.csv")
-
-                    .AllowBothDirectionEntryAtSameTime = False
-                    .TrailingStoploss = False
-                    .TickBasedStrategy = True
-                    .RuleNumber = GetComboBoxIndex_ThreadSafe(cmbRule)
-                    .RuleEntityData = Nothing
-
-                    .NumberOfTradeableStockPerDay = 5
-
-                    .NumberOfTradesPerStockPerDay = Integer.MaxValue
-
-                    .StockMaxProfitPercentagePerDay = Decimal.MaxValue
-                    .StockMaxLossPercentagePerDay = Decimal.MinValue
-
-                    .ExitOnStockFixedTargetStoploss = False
-                    .StockMaxProfitPerDay = Decimal.MaxValue
-                    .StockMaxLossPerDay = Decimal.MinValue
-
-                    .ExitOnOverAllFixedTargetStoploss = False
-                    .OverAllProfitPerDay = Decimal.MaxValue
-                    .OverAllLossPerDay = Decimal.MinValue
-
-                    .TypeOfMTMTrailing = Strategy.MTMTrailingType.None
-                    .MTMSlab = Math.Abs(.OverAllLossPerDay)
-                    .MovementSlab = .MTMSlab / 2
-                    .RealtimeTrailingPercentage = 50
-                End With
-
-                Dim filename As String = String.Format("HK Slab")
-
-                Await backtestStrategy.TestStrategyAsync(startDate, endDate, filename).ConfigureAwait(False)
-            End Using
-#End Region
-
-#Region "High Low Slab Level"
             'Using backtestStrategy As New MISGenericStrategy(canceller:=_canceller,
             '                                                  exchangeStartTime:=TimeSpan.Parse("09:15:00"),
             '                                                  exchangeEndTime:=TimeSpan.Parse("15:29:59"),
-            '                                                  tradeStartTime:=TimeSpan.Parse("9:16:00"),
+            '                                                  tradeStartTime:=TimeSpan.Parse("9:20:00"),
             '                                                  lastTradeEntryTime:=TimeSpan.Parse("14:45:59"),
             '                                                  eodExitTime:=TimeSpan.Parse("15:15:00"),
             '                                                  tickSize:=tick,
@@ -1770,7 +1714,7 @@ Public Class frmMain
             '    With backtestStrategy
             '        .StockFileName = Path.Combine(My.Application.Info.DirectoryPath, "ATR Stocklist.csv")
 
-            '        .AllowBothDirectionEntryAtSameTime = True
+            '        .AllowBothDirectionEntryAtSameTime = False
             '        .TrailingStoploss = False
             '        .TickBasedStrategy = True
             '        .RuleNumber = GetComboBoxIndex_ThreadSafe(cmbRule)
@@ -1797,10 +1741,66 @@ Public Class frmMain
             '        .RealtimeTrailingPercentage = 50
             '    End With
 
-            '    Dim filename As String = String.Format("HL Slab")
+            '    Dim filename As String = String.Format("HK Slab")
 
             '    Await backtestStrategy.TestStrategyAsync(startDate, endDate, filename).ConfigureAwait(False)
             'End Using
+#End Region
+
+#Region "High Low Slab Level"
+            Using backtestStrategy As New MISGenericStrategy(canceller:=_canceller,
+                                                              exchangeStartTime:=TimeSpan.Parse("09:15:00"),
+                                                              exchangeEndTime:=TimeSpan.Parse("15:29:59"),
+                                                              tradeStartTime:=TimeSpan.Parse("9:16:00"),
+                                                              lastTradeEntryTime:=TimeSpan.Parse("14:45:59"),
+                                                              eodExitTime:=TimeSpan.Parse("15:15:00"),
+                                                              tickSize:=tick,
+                                                              marginMultiplier:=margin,
+                                                              timeframe:=1,
+                                                              heikenAshiCandle:=False,
+                                                              stockType:=stockType,
+                                                              databaseTable:=database,
+                                                              dataSource:=sourceData,
+                                                              initialCapital:=Decimal.MaxValue / 2,
+                                                              usableCapital:=Decimal.MaxValue / 2,
+                                                              minimumEarnedCapitalToWithdraw:=Decimal.MaxValue,
+                                                              amountToBeWithdrawn:=0)
+                AddHandler backtestStrategy.Heartbeat, AddressOf OnHeartbeat
+
+                With backtestStrategy
+                    .StockFileName = Path.Combine(My.Application.Info.DirectoryPath, "ATR Stocklist.csv")
+
+                    .AllowBothDirectionEntryAtSameTime = True
+                    .TrailingStoploss = False
+                    .TickBasedStrategy = True
+                    .RuleNumber = GetComboBoxIndex_ThreadSafe(cmbRule)
+                    .RuleEntityData = Nothing
+
+                    .NumberOfTradeableStockPerDay = 5
+
+                    .NumberOfTradesPerStockPerDay = Integer.MaxValue
+
+                    .StockMaxProfitPercentagePerDay = Decimal.MaxValue
+                    .StockMaxLossPercentagePerDay = Decimal.MinValue
+
+                    .ExitOnStockFixedTargetStoploss = False
+                    .StockMaxProfitPerDay = Decimal.MaxValue
+                    .StockMaxLossPerDay = Decimal.MinValue
+
+                    .ExitOnOverAllFixedTargetStoploss = False
+                    .OverAllProfitPerDay = Decimal.MaxValue
+                    .OverAllLossPerDay = Decimal.MinValue
+
+                    .TypeOfMTMTrailing = Strategy.MTMTrailingType.None
+                    .MTMSlab = Math.Abs(.OverAllLossPerDay)
+                    .MovementSlab = .MTMSlab / 2
+                    .RealtimeTrailingPercentage = 50
+                End With
+
+                Dim filename As String = String.Format("HL Slab")
+
+                Await backtestStrategy.TestStrategyAsync(startDate, endDate, filename).ConfigureAwait(False)
+            End Using
 #End Region
 
         Catch ex As Exception
