70f1b87695a152491dd39d348e9b3cdb1c769a98
Indibar <indibar@algo2trade.com>
2020-06-03 11:16:28 +0530

    After fix

--- Backtest/Strategy Rule MIS/LowerPriceOptionBuyOnlyEODStrategyRule.vb
+++ Backtest/Strategy Rule MIS/LowerPriceOptionBuyOnlyEODStrategyRule.vb
@@ -39,13 +39,14 @@ Public Class LowerPriceOptionBuyOnlyEODStrategyRule
             End If
             If signalCandle IsNot Nothing Then
                 Dim entryPrice As Decimal = signal.Item3
-                Dim quantity As Integer = Me.LotSize
+                Dim expectedQuantity As Integer = _parentStrategy.CalculateQuantityFromTargetSL(_tradingSymbol, entryPrice, 0.05, -500, Trade.TypeOfStock.Futures)
+                Dim quantity As Integer = Math.Ceiling(expectedQuantity / Me.LotSize) * Me.LotSize
 
                 parameter = New PlaceOrderParameters With {
                                     .EntryPrice = entryPrice,
                                     .EntryDirection = Trade.TradeExecutionDirection.Buy,
                                     .Quantity = quantity,
-                                    .Stoploss = 0,
+                                    .Stoploss = 0.05,
                                     .Target = .EntryPrice + 10000000000,
                                     .Buffer = 0,
                                     .SignalCandle = signalCandle,
