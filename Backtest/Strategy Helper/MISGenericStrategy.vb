fdfe38d5a3402c36affafdff7778b0630d7d7619
Indibar <indibar@algo2trade.com>
2020-06-08 09:28:45 +0530

    After new rule

--- Backtest/Strategy Helper/MISGenericStrategy.vb
+++ Backtest/Strategy Helper/MISGenericStrategy.vb
@@ -161,6 +161,8 @@ Namespace StrategyHelper
                                             stockRule = New LossMakeupRainbowStrategyRule(XDayOneMinutePayload, stockList(stock).LotSize, Me, tradeCheckingDate, tradingSymbol, _canceller, RuleEntityData)
                                         Case 20
                                             stockRule = New AnchorSatelliteLossMakeupStrategyRule(XDayOneMinutePayload, stockList(stock).LotSize, Me, tradeCheckingDate, tradingSymbol, _canceller, RuleEntityData)
+                                        Case 21
+                                            stockRule = New AnchorSatelliteLossMakeupHKStrategyRule(XDayOneMinutePayload, stockList(stock).LotSize, Me, tradeCheckingDate, tradingSymbol, _canceller, RuleEntityData)
                                     End Select
 
                                     AddHandler stockRule.Heartbeat, AddressOf OnHeartbeat
