3000c6484aec13de8edd11725ec6b5f2928d892a
Indibar <indibar@algo2trade.com>
2020-07-21 17:05:48 +0530

    After fix

--- Algo2TradeBLL/Entities/TrendLineVeriables.vb
+++ Algo2TradeBLL/Entities/TrendLineVeriables.vb
@@ -2,4 +2,15 @@
     Public M As Decimal = Decimal.MinValue
     Public C As Decimal = Decimal.MinValue
     Public X As Decimal = Decimal.MinValue
+    Public Point1 As Date
+    Public Point2 As Date
+    Public ReadOnly Property CurrentValue
+        Get
+            If Me.M <> Decimal.MinValue AndAlso Me.C <> Decimal.MinValue AndAlso Me.X <> Decimal.MinValue Then
+                Return ((Me.M * Me.X) + Me.C)
+            Else
+                Return Decimal.MinValue
+            End If
+        End Get
+    End Property
 End Class
--- Algo2TradeBLL/Indicators/SwingHighLow.vb
+++ Algo2TradeBLL/Indicators/SwingHighLow.vb
@@ -1,5 +1,101 @@
 ﻿Namespace Indicator
     Public Module SwingHighLow
+        Public Class Swing
+            Public Property SwingHigh As Decimal
+            Public Property SwingHighTime As Date
+            Public Property SwingLow As Decimal
+            Public Property SwingLowTime As Date
+        End Class
+        Public Sub CalculateSwingHighLow(ByVal inputPayload As Dictionary(Of Date, Payload), ByVal strict As Boolean, ByRef outputPayload As Dictionary(Of Date, Swing))
+            If inputPayload IsNot Nothing AndAlso inputPayload.Count > 0 Then
+                For Each runningPayload In inputPayload.Keys
+                    Dim swingData As Swing = New Swing
+                    If strict Then
+                        If inputPayload(runningPayload).PreviousCandlePayload Is Nothing Then
+                            swingData.SwingHigh = inputPayload(runningPayload).High
+                            swingData.SwingHighTime = inputPayload(runningPayload).PayloadDate
+                            swingData.SwingLow = inputPayload(runningPayload).Low
+                            swingData.SwingLowTime = inputPayload(runningPayload).PayloadDate
+                        ElseIf inputPayload(runningPayload).PreviousCandlePayload IsNot Nothing AndAlso inputPayload(runningPayload).PreviousCandlePayload.PreviousCandlePayload Is Nothing Then
+                            If inputPayload(runningPayload).PreviousCandlePayload.High > inputPayload(runningPayload).High Then
+                                swingData.SwingHigh = inputPayload(runningPayload).PreviousCandlePayload.High
+                                swingData.SwingHighTime = inputPayload(runningPayload).PreviousCandlePayload.PayloadDate
+                            Else
+                                swingData.SwingHigh = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingHigh
+                                swingData.SwingHighTime = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingHighTime
+                            End If
+                            If inputPayload(runningPayload).PreviousCandlePayload.Low < inputPayload(runningPayload).Low Then
+                                swingData.SwingLow = inputPayload(runningPayload).PreviousCandlePayload.Low
+                                swingData.SwingLowTime = inputPayload(runningPayload).PreviousCandlePayload.PayloadDate
+                            Else
+                                swingData.SwingLow = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingLow
+                                swingData.SwingLowTime = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingLowTime
+                            End If
+                        Else
+                            If inputPayload(runningPayload).PreviousCandlePayload.High > inputPayload(runningPayload).High AndAlso
+                                inputPayload(runningPayload).PreviousCandlePayload.High > inputPayload(runningPayload).PreviousCandlePayload.PreviousCandlePayload.High Then
+                                swingData.SwingHigh = inputPayload(runningPayload).PreviousCandlePayload.High
+                                swingData.SwingHighTime = inputPayload(runningPayload).PreviousCandlePayload.PayloadDate
+                            Else
+                                swingData.SwingHigh = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingHigh
+                                swingData.SwingHighTime = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingHighTime
+                            End If
+                            If inputPayload(runningPayload).PreviousCandlePayload.Low < inputPayload(runningPayload).Low AndAlso
+                                inputPayload(runningPayload).PreviousCandlePayload.Low < inputPayload(runningPayload).PreviousCandlePayload.PreviousCandlePayload.Low Then
+                                swingData.SwingLow = inputPayload(runningPayload).PreviousCandlePayload.Low
+                                swingData.SwingLowTime = inputPayload(runningPayload).PreviousCandlePayload.PayloadDate
+                            Else
+                                swingData.SwingLow = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingLow
+                                swingData.SwingLowTime = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingLowTime
+                            End If
+                        End If
+                    Else
+                        If inputPayload(runningPayload).PreviousCandlePayload Is Nothing Then
+                            swingData.SwingHigh = inputPayload(runningPayload).High
+                            swingData.SwingHighTime = inputPayload(runningPayload).PayloadDate
+                            swingData.SwingLow = inputPayload(runningPayload).Low
+                            swingData.SwingLowTime = inputPayload(runningPayload).PayloadDate
+                        ElseIf inputPayload(runningPayload).PreviousCandlePayload IsNot Nothing AndAlso inputPayload(runningPayload).PreviousCandlePayload.PreviousCandlePayload Is Nothing Then
+                            If inputPayload(runningPayload).PreviousCandlePayload.High >= inputPayload(runningPayload).High Then
+                                swingData.SwingHigh = inputPayload(runningPayload).PreviousCandlePayload.High
+                                swingData.SwingHighTime = inputPayload(runningPayload).PreviousCandlePayload.PayloadDate
+                            Else
+                                swingData.SwingHigh = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingHigh
+                                swingData.SwingHighTime = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingHighTime
+                            End If
+                            If inputPayload(runningPayload).PreviousCandlePayload.Low <= inputPayload(runningPayload).Low Then
+                                swingData.SwingLow = inputPayload(runningPayload).PreviousCandlePayload.Low
+                                swingData.SwingLowTime = inputPayload(runningPayload).PreviousCandlePayload.PayloadDate
+                            Else
+                                swingData.SwingLow = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingLow
+                                swingData.SwingLowTime = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingLowTime
+                            End If
+                        Else
+                            If inputPayload(runningPayload).PreviousCandlePayload.High >= inputPayload(runningPayload).High AndAlso
+                                inputPayload(runningPayload).PreviousCandlePayload.High >= inputPayload(runningPayload).PreviousCandlePayload.PreviousCandlePayload.High Then
+                                swingData.SwingHigh = inputPayload(runningPayload).PreviousCandlePayload.High
+                                swingData.SwingHighTime = inputPayload(runningPayload).PreviousCandlePayload.PayloadDate
+                            Else
+                                swingData.SwingHigh = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingHigh
+                                swingData.SwingHighTime = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingHighTime
+                            End If
+                            If inputPayload(runningPayload).PreviousCandlePayload.Low <= inputPayload(runningPayload).Low AndAlso
+                                inputPayload(runningPayload).PreviousCandlePayload.Low <= inputPayload(runningPayload).PreviousCandlePayload.PreviousCandlePayload.Low Then
+                                swingData.SwingLow = inputPayload(runningPayload).PreviousCandlePayload.Low
+                                swingData.SwingLowTime = inputPayload(runningPayload).PreviousCandlePayload.PayloadDate
+                            Else
+                                swingData.SwingLow = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingLow
+                                swingData.SwingLowTime = outputPayload(inputPayload(runningPayload).PreviousCandlePayload.PayloadDate).SwingLowTime
+                            End If
+                        End If
+                    End If
+
+                    If outputPayload Is Nothing Then outputPayload = New Dictionary(Of Date, Swing)
+                    outputPayload.Add(runningPayload, swingData)
+                Next
+            End If
+        End Sub
+
         Public Sub CalculateSwingHighLow(ByVal inputPayload As Dictionary(Of Date, Payload), ByVal strict As Boolean, ByRef outputHighPayload As Dictionary(Of Date, Decimal), ByRef outputLowPayload As Dictionary(Of Date, Decimal))
             If inputPayload IsNot Nothing AndAlso inputPayload.Count > 0 Then
                 For Each runningPayload In inputPayload.Keys
