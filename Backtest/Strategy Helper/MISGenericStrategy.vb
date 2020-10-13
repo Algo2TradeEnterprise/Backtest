33b8b043232f00a864046d1e6b616c4570eb944b
Indibar <indibar@algo2trade.com>
2020-10-13 08:39:39 +0530

    After fix

--- Backtest/Strategy Helper/MISGenericStrategy.vb
+++ Backtest/Strategy Helper/MISGenericStrategy.vb
@@ -245,6 +245,8 @@ Namespace StrategyHelper
                                             stockRule = New AnchoredVWAPDirectionStrategyRule(XDayOneMinutePayload, stockList(stock).LotSize, Me, tradeCheckingDate, tradingSymbol, _canceller, RuleEntityData)
                                         Case 62
                                             stockRule = New HKReversalAdaptiveMartingaleWithDirectionStrategyRule2(XDayOneMinutePayload, stockList(stock).LotSize, Me, tradeCheckingDate, tradingSymbol, _canceller, RuleEntityData)
+                                        Case 63
+                                            stockRule = New FavourableCandleBreakoutAfterVStrategyRule(XDayOneMinutePayload, stockList(stock).LotSize, Me, tradeCheckingDate, tradingSymbol, _canceller, RuleEntityData)
                                         Case Else
                                             Throw New NotImplementedException
                                     End Select
